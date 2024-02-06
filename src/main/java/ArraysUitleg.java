1ce576d94557a87f11075c77593cdd113db020b6
Maaike <maaikejvp@hotmail.com>
2024-02-06 11:22:29 +0100

    2d arrays

--- src/main/java/ArraysUitleg.java
+++ src/main/java/ArraysUitleg.java
@@ -1,4 +1,5 @@
 public class ArraysUitleg {
+
     public static void main(String[] args) {
         int[] nrs = {3, 5, 1, 8, 7, 3};
         System.out.println(nrs[4]);
@@ -43,8 +44,30 @@ public class ArraysUitleg {
         // print alle cijfers
 
         // array gaat maken cijfers
-        // als het een oneven getal is, maak je het even (gebruik een if in de loop)
+        // als het een oneven getal is (%2!=0), maak je het even (gebruik een if in de loop)
         // print alle getallen (kan in dezelfde loop)
+
+        // multidimensional arrays
+        int[][] arrayOfIntArrays = {
+                {2, 3, 23, 56, 876, 23},
+                {1, 3, 67},
+                {8}
+        };
+
+        arrayOfIntArrays[1][2] = 22;
+
+        System.out.println(arrayOfIntArrays[2][0]);
+        System.out.println(arrayOfIntArrays[1][2]);
+
+        for(int i = 0; i < arrayOfIntArrays.length; i++) {
+            for(int j = 0; j < arrayOfIntArrays[i].length; j++) {
+                System.out.println(arrayOfIntArrays[i][j]);
+            }
+        }
+
+        // ik wil graag dat je een array met array van ints maakt
+        // en vervolgens de som van alle elementen uit die array uitrekent
+        // bonus challenge: bereken ook het gemiddelde van de ints in de array met arrays
 
 
     }
